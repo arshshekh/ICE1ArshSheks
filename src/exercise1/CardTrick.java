224b668884d85c60be1c52af7b1538c4184b0812
arshshekh <123417155+arshshekh@users.noreply.github.com>
2023-01-25 19:01:36 -0500

    Update CardTrick.java

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -5,8 +5,8 @@ package exercise1;
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
  *
- * @author dancye
- * @author Paul Bonenfant Jan 25, 2022 
+ * @author Arsh Shekh
+ * @author Arsh Shekht Jan 25, 2023
  */
 public class CardTrick {
     
@@ -57,25 +57,25 @@ public class CardTrick {
     /**
      * A simple method to print out personal information. Follow the instructions to 
      * replace this information with your own.
-     * @author Paul Bonenfant Jan 2022
+     * @author Arsh Shekh Jan 2023
      */
     private static void printInfo() {
     
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
-        System.out.println("My name is Paul, but you can call me prof, Paul or sir");
+        System.out.println("My name is Arsh Shekh, but you can call me bestie, boy or champ");
         System.out.println();
         
         System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("-- i want to become a footballer");
+        System.out.println("-- I want to build my career in sports!");
 	System.out.println();	
 
         System.out.println("My hobbies:");
         System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
+        System.out.println("-- football");
+        System.out.println("-- cricket");
         System.out.println("-- Riding my motorcycle");
 
         System.out.println();
